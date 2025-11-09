1abc067a8d4ea54cba02df649fb973aad29519fd
Leonardo Trindade <leonardotrindade1997j@gmail.com>
2025-11-09 00:49:31 -0300

    feat: Adiciona o scaffold Spring Boot com configuracoes iniciais (SRE)

---
+++ src/main/java/com/hitch/conselheiroamoroso/HitchConselheiroAmorosoApplication.java
@@ -0,0 +1,13 @@
+package com.hitch.conselheiroamoroso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitchConselheiroAmorosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitchConselheiroAmorosoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hitch/conselheiroamoroso/HitchConselheiroAmorosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitch.conselheiroamoroso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitchConselheiroAmorosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
